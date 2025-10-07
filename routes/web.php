fbcfab9c8abbd362c20410def8e4ba7d2b92c53b
Benediktus Hosni I <37971110+reivnick@users.noreply.github.com>
2025-10-07 20:04:53 +0700

    Update

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('IdCardGenerator');
+    return Inertia::render('idCardGenerator');
 })->name('home');
 
 //Route::get('/id-card', function () {
